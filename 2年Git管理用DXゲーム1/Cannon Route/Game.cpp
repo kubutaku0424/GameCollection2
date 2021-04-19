b74141f51df4a3f6eb19eb6cac3cce9b3163d246
kubutaku0424 <takuyuu0839@outlook.jp>
2021-04-19 22:59:58 +0900

    　大砲の画像を追加しました

--- 2年Git管理用DXゲーム1/Cannon Route/Game.cpp
+++ 2年Git管理用DXゲーム1/Cannon Route/Game.cpp
@@ -3,16 +3,14 @@
 #include "SceneMgr.h"
 #include "DxLib.h"
 
-static int mImageHandle;    //画像ハンドル格納用変数
-
+int cannon_image;
 //初期化
 void Game_Initialize() {
-    mImageHandle = LoadGraph("images/Scene_Game.png");    //画像のロード
+    cannon_image= LoadDivGraph("image/cannon.png");    //画像のロード
 }
 
 //終了処理
 void Game_Finalize() {
-    DeleteGraph(mImageHandle);    //画像の解放
 }
 
 //更新
@@ -24,7 +22,7 @@ void Game_Update() {
 
 //描画
 void Game_Draw() {
-    DrawGraph(0, 0, mImageHandle, FALSE);
+    DrawGraph(0, 0, cannon_image, TRUE);
     DrawString(0, 0, "ゲーム画面です。", GetColor(255, 255, 255));
     DrawString(0, 20, "Escキーを押すとメニュー画面に戻ります。", GetColor(255, 255, 255));
 }
